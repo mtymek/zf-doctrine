894ede5cf57dfb35e79457c8ec4c958601c531dd
Mateusz Tymek <mtymek@gmail.com>
2011-10-29 16:21:52 +0200

    Integrated Doctrine

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,17 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    public function _initDoctrine()
+    {
+        $doctrineConfig = $this->getOption('doctrine');
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+        $conn = Doctrine_Manager::connection($doctrineConfig['dsn'],'doctrine');
+        $conn->setAttribute(Doctrine_Core::ATTR_USE_NATIVE_ENUM, true);
+        return $conn;
+    }
 
+  
+  
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,16 +1,15 @@
 <?php
 
 class IndexController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+{    
 
     public function indexAction()
     {
-        // action body
+        // create new user
+        $user = new App_Model_User();
+        $user->email = 'test@domain.tld';
+        $user->password = sha1('password');
+        $user->save();
     }
 
 
